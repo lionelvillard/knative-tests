2ea94096a018d829fa82585961980a5e47ea0545
Lionel Villard <villard@us.ibm.com>
2020-10-05 13:56:08 -0400

    another try

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 )
 
@@ -11,7 +12,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
+func ip() {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	fmt.Println(localAddr)
+}
+
 func main() {
+	ip()
 	http.HandleFunc("/", handler)
 	fmt.Println("starting server")
 	log.Fatal(http.ListenAndServe(":8080", nil))
